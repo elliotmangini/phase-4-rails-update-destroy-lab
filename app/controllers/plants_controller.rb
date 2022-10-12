e4726e6bfc5ba840c3355b8f7e506e82ab24b6e1
Elliot Mangini <elliot.mangini@gmail.com>
2022-10-12 10:52:45 -0600

    10 out of 10

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -8,8 +8,10 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
+  rescue ActiveRecord::RecordNotFound
+    render_not_found
   end
 
   # POST /plants
@@ -18,9 +20,34 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant
+  rescue ActiveRecord::RecordNotFound
+    render_not_found
+  end
+
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  rescue ActiveRecord::RecordNotFound
+    render_not_found
+  end
+
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def find_plant
+    Plant.find_by(id: params[:id])
+  end
+
+  def render_not_found
+    render json: { error: "Plant not found."}, status: :render_not_found
   end
 end
